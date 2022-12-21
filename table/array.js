d1f194f058cfebac8c7c0bf4457393b0f27a5f81
nekashtanka <88548963+nekashtanka@users.noreply.github.com>
2022-12-21 14:37:53 +0300

    array table

---
+++ table/array.js
@@ -0,0 +1,18 @@
+const subscribers = [
+    {
+        registrationDate: "02.06.2022",
+        registrationTime: "22:20:00",
+        userID: "78523972",
+        firstName: "Карина",
+        lasrName: "Трофимова",
+        username: "@nekashtanka",
+    },
+    {
+        registrationDate: "22.06.2022",
+        registrationTime: "00:20:00",
+        userID: "46930862",
+        firstName: "Николай",
+        lasrName: "Николаев",
+        username: "@лох",
+    }
+]
