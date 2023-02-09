1112f35aa126960920e441b8f8709c3ce31fbd26
Houston1304 <64915948+Houston1304@users.noreply.github.com>
2023-02-09 22:22:44 +0300

    table_password

--- table/tableScript.js
+++ table/tableScript.js
@@ -17,61 +17,63 @@ const subscribers = [
   },
 ];
 
-const dateBox = document.createElement("div");
-dateBox.className = "dateBox";
+function createFilter() {
+  const dateBox = document.createElement("div");
+  dateBox.className = "dateBox";
 
-const startDate = document.createElement("input");
-startDate.type = "date";
-startDate.className = "startDate";
+  const startDate = document.createElement("input");
+  startDate.type = "date";
+  startDate.className = "startDate";
 
-const endDate = document.createElement("input");
-endDate.type = "date";
-endDate.className = "endDate";
+  const endDate = document.createElement("input");
+  endDate.type = "date";
+  endDate.className = "endDate";
 
-const dash = document.createElement("label");
-dash.className = "dash";
-dash.textContent = " - ";
+  const dash = document.createElement("label");
+  dash.className = "dash";
+  dash.textContent = " - ";
 
-const searchDate = document.createElement("button");
-searchDate.className = "searchDate";
-searchDate.textContent = "Отфильтровать";
+  const searchDate = document.createElement("button");
+  searchDate.className = "searchDate";
+  searchDate.textContent = "Отфильтровать";
 
-searchDate.addEventListener("click", () => {
-  if (document.querySelector(".backTTF")) {
-    document.querySelector(".backTTF").remove();
-  }
-  const tr = document.getElementsByClassName("database");
+  searchDate.addEventListener("click", () => {
+    if (document.querySelector(".backTTF")) {
+      document.querySelector(".backTTF").remove();
+    }
+    const tr = document.getElementsByClassName("database");
 
-  const backTTF = document.createElement("button");
-  backTTF.textContent = "Назад";
-  backTTF.className = "backTTF";
-  document.body.prepend(backTTF);
+    const backTTF = document.createElement("button");
+    backTTF.textContent = "Назад";
+    backTTF.className = "backTTF";
+    document.body.prepend(backTTF);
 
-  backTTF.addEventListener("click", () => {
-    window.location.href = "table.html";
+    backTTF.addEventListener("click", () => {
+      window.location.href = "table.html";
+    });
+
+    for (var i = 0; i < tr.length; i++) {
+      var td = tr[i].getElementsByClassName("date")[0];
+      let td_date = td.innerHTML;
+      td_date =
+        td_date.substr(6, 4) +
+        "-" +
+        td_date.substr(3, 2) +
+        "-" +
+        td_date.substr(0, 2);
+
+      let start = startDate.value;
+      let end = endDate.value;
+      if (td_date < start || end < td_date) {
+        tr[i].style.position = "absolute";
+        tr[i].style.visibility = "hidden";
+      }
+    }
   });
 
-  for (var i = 0; i < tr.length; i++) {
-    var td = tr[i].getElementsByClassName("date")[0];
-    let td_date = td.innerHTML;
-    td_date =
-      td_date.substr(6, 4) +
-      "-" +
-      td_date.substr(3, 2) +
-      "-" +
-      td_date.substr(0, 2);
-
-    let start = startDate.value;
-    let end = endDate.value;
-    if (td_date < start || end < td_date) {
-      tr[i].style.position = "absolute";
-      tr[i].style.visibility = "hidden";
-    }
-  }
-});
-
-document.body.append(dateBox, searchDate);
-dateBox.append(startDate, dash, endDate);
+  document.body.append(dateBox, searchDate);
+  dateBox.append(startDate, dash, endDate);
+}
 
 function createTable() {
   const mainTable = document.createElement("table");
@@ -141,7 +143,33 @@ function createTable() {
   mainTable.append(tbody);
   document.body.append(mainTable);
 }
-createTable();
+
+const popUp = document.createElement("div");
+popUp.className = "popUp";
+
+const passwordForm = document.createElement("form");
+passwordForm.className = "passwordForm";
+
+const passwordInput = document.createElement("input");
+passwordInput.id = "passwordInput";
+passwordInput.placeholder = "Введите пароль..";
+passwordInput.type = "number";
+
+const confirmPassword = document.createElement("button");
+confirmPassword.id = "confirmPassword";
+confirmPassword.textContent = "Подтвердить";
+
+passwordForm.append(passwordInput, confirmPassword);
+popUp.append(passwordForm);
+document.body.append(popUp);
+
+passwordForm.addEventListener("submit", () => {
+  if (passwordInput.value == 123) {
+    createFilter();
+    createTable();
+    popUp.remove();
+  }
+});
 
 var getCellValue = function (tr, idx) {
   return tr.children[idx].innerText || tr.children[idx].textContent;
