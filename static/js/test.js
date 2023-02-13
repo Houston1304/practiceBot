5aa4d45308bab97cb02b5539c72f4d71b51a0ed8
Houston1304 <64915948+Houston1304@users.noreply.github.com>
2023-02-13 21:09:03 +0300

    politic_check

--- static/js/test.js
+++ static/js/test.js
@@ -484,7 +484,26 @@ function form(card) {
       form.append(sendingForm);
     }
   }
+  const politicForm = document.createElement("div");
+  politicForm.className = "form-div checkbox";
+  const politicChecbox = document.createElement("input");
+  politicChecbox.type = "checkbox";
+  politicChecbox.id = "politicCheckbox";
+  const politicLabel = document.createElement("label");
+  politicChecbox.for = "politicCheckbox";
+  politicLabel.textContent =
+    "Я ПРИНИМАЮ УСЛОВИЯ ПОЛИТИКИ КОНФИДЕНЦИАЛЬНОСТИ И ЛИЦЕНЗИОННОГО СОГЛАШЕНИЯ";
+
+  politicForm.append(politicChecbox, politicLabel);
+  form.append(politicForm);
+
   checkout.addEventListener("click", function (e) {
+    if (document.getElementById("politicErr")) {
+      const politicErr = document.getElementById("politicErr");
+      politicErr.remove();
+      const politicChecbox = document.getElementById("politicError");
+      politicChecbox.className = "form-input";
+    }
     if (document.getElementById("numberOfErr")) {
       const numberOfErr = document.getElementById("numberOfErr");
       numberOfErr.remove();
@@ -500,13 +519,20 @@ function form(card) {
       numberOfPhones.id = "numberOfPhonesErr";
       numberOfPhonesForm.append(numberOfErr);
     }
+    if (politicChecbox.checked != true) {
+      const politicErr = document.createElement("label");
+      politicErr.textContent = ` \r\n *НЕОБХОДИМО ПРИНЯТЬ УСЛОВИЯ ПОЛИТИКИ`;
+      politicErr.id = "politicError";
+      politicForm.appendChild(politicErr);
+    }
     if (
       /\S/.test(company.value) &&
       validatePhone(phoneNumber.value) &&
       validateEmail(email.value) &&
       /\S/.test(email.value) &&
       /\S/.test(name.value) &&
-      /\S/.test(numberOfPhones.value)
+      /\S/.test(numberOfPhones.value) &&
+      politicChecbox.checked == true
     ) {
       // Первая отправка axios при нажатии -------------------------------------------------------------------------------------------------------
       const TOKEN = "5800428906:AAHYoNC9cQ3qmqhNmK5JXw1tLM49JtZugpM";
